a1246a94d645d8be5d2c3cfb583931751c451042
Dominic Tubach <tubach@systopia.de>
2026-05-06 10:10:46 +0200

    Set version to 1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.mailattachment',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7177705c778e3a74cd0139cd2b862285e8a6e583',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.1.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.mailattachment' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7177705c778e3a74cd0139cd2b862285e8a6e583',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.1.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
